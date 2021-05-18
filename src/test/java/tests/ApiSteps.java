4b88fc76985fb01f9a0cfc60f32c241a949188be
jul112 <julia.bv@gmail.com>
2021-05-18 22:01:53 +0300

    Added Tests: getStatusNotFoundForMissingUser/successfulUserRegistration/successfulLogin

--- src/test/java/tests/ApiSteps.java
+++ src/test/java/tests/ApiSteps.java
@@ -2,42 +2,74 @@ package tests;
 
 import io.qameta.allure.Step;
 import io.qameta.allure.restassured.AllureRestAssured;
-import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
 
 public class ApiSteps {
-    final DataUser user = new DataUser();
 
     @Step("Create user")
-    public DataUser createUser(String name, String job) {
-        user.setName(name);
-        user.setJob(job);
-        return given()
-                    .filter(new AllureRestAssured())
-                    .baseUri("https://reqres.in/api")
-                    .body(user)
-                .when()
-                     .post("/users")
-                .then()
-                    .statusCode(201)
-                .extract()
-                .as(DataUser.class);
-    }
-
-    @Step("Update user data")
-    public DataUser updateUserData(String job, String id) {
-        user.setId(id);
-        user.setJob(job);
+    public Response createUser(String name, String job) {
         return given()
                 .filter(new AllureRestAssured())
-                .body(user)
+                .contentType(JSON)
+                .body("{ \"name\": \""+name+"\", " + "\"job\": \""+job+"\" }")
                 .when()
-                .contentType(ContentType.JSON)
-                .patch("/users/2")
-        .then()
+                .post("/api/users")
+                .then()
+                .statusCode(201)
+                .extract().response();
+    }
+
+    @Step("Update user data with PATCH")
+    public Response updateUserDataWithPatch(String name, String job) {
+       return given()
+                .filter(new AllureRestAssured())
+                .contentType(JSON)
+                .body("{ \"name\": \""+name+"\", " + "\"job\": \""+job+"\" }")
+                .when()
+                .patch("/api/users/2")
+                .then()
                 .statusCode(200)
-                .extract()
-                .as(DataUser.class);
+                .extract().response();
+    }
+
+    @Step("GET user's data")
+    public Response getUserData(int userId, int status) {
+        return given()
+                .filter(new AllureRestAssured())
+                .contentType(JSON)
+                .when()
+                .get("/api/users/" + userId)
+                .then()
+                .statusCode(status)
+                .extract().response();
+    }
+
+    @Step("Register new user")
+    public Response registerUser(String email, String password, int statusCode) {
+        return given()
+                .filter(new AllureRestAssured())
+                .contentType(JSON)
+                .body("{ \"email\": \""+email+"\", " + "\"password\": \""+password+"\" }")
+                .when()
+                .post("/api/register")
+                .then()
+                .statusCode(statusCode)
+                .extract().response();
+    }
+
+    @Step("Authorization")
+    public Response login(String email, String password, int statusCode) {
+        return given()
+                .filter(new AllureRestAssured())
+                .contentType(JSON)
+                .body("{ \"email\": \""+email+"\", " + "\"password\": \""+password+"\" }")
+                .when()
+                .post("/api/login")
+                .then()
+                .statusCode(statusCode)
+                .extract().response();
     }
 }
--- src/test/java/tests/ApiTests.java
+++ src/test/java/tests/ApiTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,27 +10,56 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ApiTests {
     private final ApiSteps apiSteps = new ApiSteps();
-    private static final String NAME = "testusername";
+
+    private static final String NAME = "monk";
     private static final String JOB = "janitor";
-    private static final String NEW_JOB= "president";
-    private static final String ID = "2";
+    private static final String NEW_JOB = "president";
+    private static final String EMAIL = "eve.holt@reqres.in";
+    private static final String REGISTER_PASSWORD = "pistol";
+    private static final String LOGIN_PASSWORD = "cityslicka";
+    private static final String TOKEN = "QpwL5tke4Pnpja7X4";
+    private static final int MISSING_USER_ID = 23;
+    private static final int STATUS_NOT_FOUND = 404;
+    private static final int STATUS_SUCCESS = 200;
+
 
     @BeforeEach
     public void setBaseUri() {
-        RestAssured.baseURI = "https://reqres.in/api";
+        RestAssured.baseURI = "https://reqres.in";
     }
 
     @Test
     @DisplayName("Create user from API")
     public void createUserFromAPI() {
-        final DataUser created = apiSteps.createUser(NAME, JOB);
-        assertThat(created.getId()).isNotNull();
+        Response createUserResponse = apiSteps.createUser(NAME, JOB);
+        assertThat(createUserResponse.jsonPath().getString("id")).isNotNull();
     }
 
     @Test
-    @DisplayName("Update user data from API")
-    public void updateUserDataFromAPI() {
-        final DataUser updated = apiSteps.updateUserData(NEW_JOB, ID);
-        assertThat(updated.getJob()).isEqualTo(NEW_JOB);
+    @DisplayName("Update user's job from API with PATCH")
+    public void updateUserJobWithPatch() {
+        Response updateDataResponse = apiSteps.updateUserDataWithPatch(NAME, NEW_JOB);
+        assertThat(updateDataResponse.jsonPath().getString("job")).isEqualTo(NEW_JOB);
+    }
+
+    @Test
+    @DisplayName("Get response status code ${NOT_FOUND_STATUS} from data of missing user")
+    public void getStatusNotFoundForMissingUser() {
+        Response getStatusResponse = apiSteps.getUserData(MISSING_USER_ID, STATUS_NOT_FOUND);
+        assertThat(getStatusResponse.getStatusCode()).isEqualTo(404);
+    }
+
+    @Test
+    @DisplayName("Successful user registration")
+    public void successfulUserRegistration() {
+        Response registerUserResponse = apiSteps.registerUser(EMAIL, REGISTER_PASSWORD, STATUS_SUCCESS);
+        assertThat(registerUserResponse.jsonPath().getString("token")).isEqualTo(TOKEN);
+    }
+
+    @Test
+    @DisplayName("Authorization")
+    public void successfulLogin() {
+        Response loginUserResponse = apiSteps.login(EMAIL, LOGIN_PASSWORD, STATUS_SUCCESS);
+        assertThat(loginUserResponse.jsonPath().getString("token")).isEqualTo(TOKEN);
     }
 }
